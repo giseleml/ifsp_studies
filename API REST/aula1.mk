a30227c8f7242e6bf4965d1cfea4fd4fea93478a
giseleml <giselemaraleo@gmail.com>
2020-06-06 19:58:50 -0300

    aula 1

---
+++ API REST/aula1.mk
@@ -0,0 +1,19 @@
+# Aula 1 
+
+### O que são API's? 
+Interface mediadora para interagir com outras aplicações. 
+É possível se comunicar com uma ampla gama de sistemas
+através das API's. 
+
+### O que são Serviços?
+São mecanismos que oferecem os dados internos de uma
+aplicação de forma interoperável, ou seja, de forma que 
+esses dados possam ser consumidos por outras aplicações
+que não necessariamente são escritas na mesma linguagem.
+- Interoperável: capacidade de um sistema de se comunicar
+de forma transparente com outro sistema semelhante ou não.
+
+Para um serviço fornecer o máximo de portabilidade, 
+é necessário que utilize protocolos que sejam facilmente 
+entendidos por várias aplicações. Exemplo: HTTP.
+
